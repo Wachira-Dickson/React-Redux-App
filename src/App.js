461078331ca74a1a6969aefb7bf609aca1587f1f
Your Name <dicksonwachira886@gmail.com>
2025-05-26 21:01:53 +0300

    Create the create project component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 class App extends Component {
   render() {
@@ -17,6 +18,7 @@ class App extends Component {
             <Route path='/project/:id' element={<ProjectDetails />} />
             <Route path='/signin' element={< SignIn/>} />
             <Route path='/signup' element={< SignUp />} />
+            <Route path='/create' element={< CreateProject />} />
           </Routes>
         </div>
       </BrowserRouter>
