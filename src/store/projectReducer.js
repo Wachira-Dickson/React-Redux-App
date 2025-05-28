6cd96c1064998aeab50cf161f955a123c6b7c221
Your Name <dicksonwachira886@gmail.com>
2025-05-28 10:56:11 +0300

    Add dummy data for the project contents and specifications

--- src/store/projectReducer.js
+++ src/store/projectReducer.js
@@ -1,7 +1,13 @@
-const initState = {}
+const initState = {
+    projects:[  
+        {id: 1, title: 'I want money', content: 'I want to earn money', author: 'John Doe'},
+        {id: 2, title: 'I want to travel', content: 'I want to travel the world', author: 'Jane Doe'},
+        {id: 3, title: 'I want to learn', content: 'I want to learn new skills', author: 'Alice Smith'}
+    ]
+}
 
 const projectReducer = (state = initState, action) => {
     return state;
 }
 
-export default projectReducer;
+export default projectReducer
