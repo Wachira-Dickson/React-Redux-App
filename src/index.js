5f6419898de94c5ccec41a77e0cfebddc408bcf8
Your Name <dicksonwachira886@gmail.com>
2025-05-28 11:39:11 +0300

    Async code with redux and use thunk for the projects

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './store/reducers/rootReducer';
 
+
 // Create Redux store
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
