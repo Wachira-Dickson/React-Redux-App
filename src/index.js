1d5273820d814da1a4ac316faba8f9525adf2e63
Your Name <dicksonwachira886@gmail.com>
2025-05-28 14:44:42 +0300

    Connect redux to Firebase as well as the fbConfig

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig'; 
 
 import './index.css';
 import App from './App';
@@ -11,7 +14,10 @@ import rootReducer from './store/reducers/rootReducer';
 
 
 // Create Redux store
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, compose( applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+  reduxFirestore(fbConfig), // Add reduxFirestore to the compose function
+  reactReduxFirebase(fbConfig) // Add reactReduxFirebase to the compose function  
+));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
